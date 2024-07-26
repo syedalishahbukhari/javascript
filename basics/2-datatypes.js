2369b2560224f4364e27937b71e431a13baacc16
syedalishahbukhari <syedali123367@gmail.com>
2024-07-26 18:09:24 +0000

    datatypes

---
+++ basics/2-datatypes.js
@@ -0,0 +1,18 @@
+"use strict";
+console.log(3+3)
+console.log("ali bukhari")
+ let name="ali";
+ let age = 19
+ let IslogginIn = false
+ 
+// number => 2 to power 52
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone
+// undefined =>
+// symbol => unique
+
+// object
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
