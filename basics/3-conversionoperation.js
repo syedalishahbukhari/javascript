178ca186c1dbadf31f758f1210a05cb6c73dd707
syedalishahbukhari <syedali123367@gmail.com>
2024-07-28 17:58:40 +0000

    conversion of datatypes

---
+++ basics/3-conversionoperation.js
@@ -0,0 +1,22 @@
+let age = false
+// console.log(typeof age)
+let ValueInNumber = Number(age)
+console.log(typeof ValueInNumber)
+console.log(ValueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// True => 1; False => 0
+
+let IsLoogedIn = "ali"
+let BooleanIsLogged = (Boolean(IsLoogedIn))
+console.log(BooleanIsLogged)
+
+// 1 => true; 0 => false
+// "" => false
+// "ali" => true
+
+let StringNumber = 33
+let SomeNumber = String(StringNumber)
+console.log(SomeNumber)
+console.log(typeof SomeNumber)
