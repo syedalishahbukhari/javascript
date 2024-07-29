58cf7a5c6cec27dbbd78e2f0b6686abd9f34f30a
syedalishahbukhari <syedali123367@gmail.com>
2024-07-29 18:47:26 +0000

    datatypes-summery

---
+++ Datatypes-summery.js
@@ -0,0 +1,25 @@
+// premitive : 
+// 7 types: string , number , null , undefined , boolean , BigInt , symbol
+
+// Reference : 
+// Array , Objects , functions
+
+const score = 100
+const scorevalue = 100.3
+
+const IdLoggedIn = false
+const outsidetemp = null
+let Email;
+const id= Symbol('123')
+const anotherid = Symbol('123')
+// console.log(id === anotherid)
+
+const countries = ["pakistan" , "UAE" , "iraq"]
+let myobj = {
+    name:"ali",
+    age:19
+}
+const MyFunction = function(){
+    console.log("hello ali")
+}
+console.log(typeof countries)
